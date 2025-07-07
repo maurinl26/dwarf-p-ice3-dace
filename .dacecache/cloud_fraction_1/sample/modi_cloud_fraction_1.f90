ddf9e11a00d0895da93ce93dda4bbbdf55867fb3
Loïc Maurin <loicmaurin@Mac-mini-de-Loic.local>
2025-07-08 01:21:42 +0200

    interface and test for cloud_fraction_1

--- .dacecache/cloud_fraction_1/sample/modi_cloud_fraction_1.f90
+++ .dacecache/cloud_fraction_1/sample/modi_cloud_fraction_1.f90
@@ -6,53 +6,51 @@ module modi_thermo
     implicit none
 
     interface
-        type(c_funptr) function c_dace_init_thermodynamic_fields(I, J, K, NRR) bind(c, name='__dace_init_thermodynamic_fields')
+        type(c_funptr) function c_dace_init_cloud_fraction_1(I, J, K) bind(c, name='__dace_init_cloud_fraction_1')
             use, intrinsic :: iso_c_binding, only: c_int, c_funptr
-            integer(c_int), intent(in) :: I
-            integer(c_int), intent(in) :: J
-            integer(c_int), intent(in) :: K
-            integer(c_int), intent(in) :: NRR
-        end function c_dace_init_thermodynamic_fields
+            integer(c_int), value :: I
+            integer(c_int), value :: J
+            integer(c_int), value :: K
+        end function c_dace_init_cloud_fraction_1
 
-        subroutine c_program_thermodynamic_fields(handle, cph, exn, ls, lv, rc, rg, ri, rr, rs, rv, t, th, CI, CL, CPD,&
-                &CPV, I, J, K, LSTT, LVTT, NRR, TT) bind(c, name='__program_thermodynamic_fields')
+        subroutine c_program_cloud_fraction_1(handle, cph, exnref, ls, lv, rc, rc_tmp, rcs0, rcs1, ri, ri_tmp, ris0,&
+                &ris1, rvs0, rvs1, ths0, ths1, I, J, K, &
+                &dt) bind(c, name='__program_cloud_fraction_1')
 
             use, intrinsic :: iso_c_binding, only: c_funptr, c_ptr, c_float, c_int, c_bool, c_double
 
             type(c_funptr), value :: handle
 
-            real(c_double):: cph(*)
-            real(c_double):: exn(*)
-            real(c_double):: ls(*)
-            real(c_double):: lv(*)
-            real(c_double):: rc(*)
-            real(c_double):: rg(*)
-            real(c_double):: ri(*)
-            real(c_double):: rr(*)
-            real(c_double):: rs(*)
-            real(c_double):: rv(*)
-            real(c_double):: t(*)
-            real(c_double):: th(*)
+            real(c_double) :: cph(*)
+            real(c_double) :: exnref(*)
+            real(c_double) :: ls(*)
+            real(c_double) :: lv(*)
+            real(c_double) :: rc(*)
+            real(c_double) :: rc_tmp(*)
+            real(c_double) :: rcs0(*)
+            real(c_double) :: rcs1(*)
+            real(c_double) :: ri(*)
+            real(c_double) :: ri_tmp(*)
+            real(c_double) :: ris0(*)
+            real(c_double) :: ris1(*)
+            real(c_double) :: rvs0(*)
+            real(c_double) :: rvs1(*)
+            real(c_double) :: ths0(*)
+            real(c_double) :: ths1(*)
 
-            real(c_double), value :: CI
-            real(c_double), value :: CL
-            real(c_double), value :: CPD
-            real(c_double), value :: CPV
             integer(c_int), value :: I
             integer(c_int), value :: J
             integer(c_int), value :: K
-            real(c_double), value :: LSTT
-            real(c_double), value :: LVTT
-            integer(c_int), value :: NRR
-            real(c_double), value :: TT
+            real(c_double), value :: dt
 
-        end subroutine c_program_thermodynamic_fields
 
-        subroutine c_dace_exit_thermodynamic_fields(handle, err) bind(c, name='__dace_exit_thermodynamic_fields')
+        end subroutine c_program_cloud_fraction_1
+
+        subroutine c_dace_exit_cloud_fraction_1(handle, err) bind(c, name='__dace_exit_cloud_fraction_1')
             use, intrinsic :: iso_c_binding, only: c_funptr, c_int
             type(c_funptr), intent(in) :: handle
             integer(c_int), intent(out) :: err
-        end subroutine c_dace_exit_thermodynamic_fields
+        end subroutine c_dace_exit_cloud_fraction_1
 
     end interface
 
--- .dacecache/cloud_fraction_1/sample/test_cloud_fraction_1.f90
+++ .dacecache/cloud_fraction_1/sample/test_cloud_fraction_1.f90
@@ -5,70 +5,72 @@ program main
     use modi_thermo
     use, intrinsic :: iso_c_binding
 
-    integer(c_int) :: I, J, K, NRR
+    integer(c_int) :: I, J, K
     type(c_funptr) :: handle
 
-    real(c_double) :: CI = 1.0
-    real(c_double) :: CL = 1.0
-    real(c_double) :: CPD = 1.0
-    real(c_double) :: CPV = 1.0
-    real(c_double) :: LSTT = 1.0
-    real(c_double) :: LVTT = 1.0
-    real(c_double) :: TT = 1.0
+    real(c_double) :: dt = 50.0
+
 
     real(c_double), allocatable :: cph(:,:,:)
-    real(c_double), allocatable :: exn(:,:,:)
+    real(c_double), allocatable :: exnref(:,:,:)
     real(c_double), allocatable :: ls(:,:,:)
     real(c_double), allocatable :: lv(:,:,:)
-    real(c_double), allocatable :: rc(:,:,:)
-    real(c_double), allocatable :: rg(:,:,:)
-    real(c_double), allocatable :: ri(:,:,:)
-    real(c_double), allocatable :: rr(:,:,:)
-    real(c_double), allocatable :: rs(:,:,:)
-    real(c_double), allocatable :: rv(:,:,:)
-    real(c_double), allocatable :: t(:,:,:)
-    real(c_double), allocatable :: th(:,:,:)
-
-    I = 15
-    J = 15
-    K = 15
-    NRR = 6
+    real(c_double), allocatable :: rc0(:,:,:)
+    real(c_double), allocatable :: rc_tmp(:,:,:)
+    real(c_double), allocatable :: rcs0(:,:,:)
+    real(c_double), allocatable :: rcs1(:,:,:)
+    real(c_double), allocatable :: ri0(:,:,:)
+    real(c_double), allocatable :: ri_tmp(:,:,:)
+    real(c_double), allocatable :: ris0(:,:,:)
+    real(c_double), allocatable :: ris1(:,:,:)
+    real(c_double), allocatable :: rvs0(:,:,:)
+    real(c_double), allocatable :: rvs1(:,:,:)
+    real(c_double), allocatable :: ths0(:,:,:)
+    real(c_double), allocatable :: ths1(:,:,:)
 
     allocate(cph(I, J, K))
-    allocate(exn(I, J, K))
+    allocate(exnref(I, J, K))
     allocate(ls(I, J, K))
     allocate(lv(I, J, K))
-    allocate(rc(I, J, K))
-    allocate(rg(I, J, K))
-    allocate(ri(I, J, K))
-    allocate(rr(I, J, K))
-    allocate(rs(I, J, K))
-    allocate(rv(I, J, K))
-    allocate(t(I, J, K))
-    allocate(th(I, J, K))
+    allocate(rc0(I, J, K))
+    allocate(rc_tmp(I, J, K))
+    allocate(rcs0(I, J, K))
+    allocate(rcs1(I, J, K))
+    allocate(ri0(I, J, K))
+    allocate(ri_tmp(I, J, K))
+    allocate(ris0(I, J, K))
+    allocate(ris1(I, J, K))
+    allocate(rvs0(I, J, K))
+    allocate(rvs1(I, J, K))
+    allocate(ths0(I, J, K))
+    allocate(ths1(I, J, K))
 
-    cph(:,:,:) = 0.0
-    exn(:,:,:) = 1.0
-    ls(:,:,:) = 0.0
-    lv(:,:,:) = 0.0
-    rc(:,:,:) = 1.0
-    rg(:,:,:) = 1.0
-    ri(:,:,:) = 1.0
-    rr(:,:,:) = 1.0
-    rs(:,:,:) = 1.0
-    rv(:,:,:) = 1.0
-    t(:,:,:) = 0.0
-    th(:,:,:) = 1.0
+    cph(:,:,:) = 1.0
+    exnref(:,:,:) = 1.0
+    ls(:,:,:) = 1.0
+    lv(:,:,:) = 1.0
+    rc0(:,:,:) = 1.0
+    rc_tmp(:,:,:) = 1.0
+    rcs0(:,:,:) = 1.0
+    rcs1(:,:,:) = 1.0
+    ri0(:,:,:) = 1.0
+    ri_tmp(:,:,:) = 1.0
+    ris0(:,:,:) = 1.0
+    ris1(:,:,:) = 0.0
+    rvs0(:,:,:) = 1.0
+    rvs1(:,:,:) = 0.0
+    ths0(:,:,:) = 1.0
+    ths1(:,:,:) = 0.0
 
-    handle = c_dace_init_thermodynamic_fields(I, J, K, NRR)
+    handle = c_dace_init_cloud_fraction_1(I, J, K)
 
-    call c_program_thermodynamic_fields(handle,&
-        &cph, exn, ls, lv, rc, rg, ri, rr, rs, rv, t, th,&
-        &CI, CL, CPD, CPV, I, J, K, LSTT, LVTT, NRR, TT)
+    call c_program_cloud_fraction_1(handle, cph, exnref, ls, lv,&
+            &rc, rc_tmp, rcs0, rcs1, ri, ri_tmp, ris0,&
+            &ris1, rvs0, rvs1, ths0, ths1, I, J, K, &
+            &dt)
 
-    print *, "cph :", sum(cph)/(I * J * K)
-    print *, "ls :", sum(ls)/(I * J * K)
-    print *, "lv :", sum(lv)/(I * J * K)
-    print *, "t :", sum(t)/(I * J * K)
+    print *, "rvs1 :", sum(rvs1)/(I * J * K)
+    print *, "rcs1 :", sum(rcs1)/(I * J * K)
+    print *, "ris1 :", sum(ris1)/(I * J * K)
 
 end program main
